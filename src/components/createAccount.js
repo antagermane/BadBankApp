9f74c744277ee72767823f26459808ea86001c4e
antagermane <82213115+antagermane@users.noreply.github.com>
2021-10-04 23:18:36 +0100

    Add files via upload

--- src/components/createAccount.js
+++ src/components/createAccount.js
@@ -3,29 +3,42 @@ import UserContext from './context'
 
 function CreateAccount(){
   const [show, setShow] = React.useState(true);
-  const [status, setStatus] = React.useState(true);
-  const [name, setName] = React.useState(true);
-  const [email, setEmail] = React.useState(true);
-  const [password, setPassword] = React.useState(true);
+  const [status, setStatus] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const isEnabladed = fieldsUsed()
   const ctx = React.useContext(UserContext);
+  
+
+  function fieldsUsed(){
+    return(
+      name.length > 0 || email.length > 0 || password.length > 0
+    )
+  }
 
   function validate(field,label){
     if (!field) {
-      setStatus('Error: ' + label);
-      setTimeout(() => setStatus(''),3000);
+        setStatus('Enter ' + label);
+        setTimeout(() => setStatus(''),3000);
+        return false;
+      }
+    if (password.length <= 7){
+      setStatus('Password must be at least 8 characters long')
       return false;
     }
-    return true;
-  }
+      return true;
+    }
+
 
   function handleCreate(){
     console.log(name,email,password);
     if (!validate(name, 'name')) return;
     if (!validate(email, 'email')) return;
     if (!validate(password, 'password')) return;
-    ctx.users.push({name,email,password,balance:0})
+    ctx.users.push({name,email,password})
     setShow(false);
-
+    setStatus('');
   }
 
   function clearForm(){
@@ -35,11 +48,15 @@ function CreateAccount(){
     setShow(true);
   }
 
-  return (
-    //status={status},
+  return(
+
+    
     <div className="card mb-3" style={{maxWidth: "540px", backgroundColor: 'rgb(230, 255, 238'}}>
       <div class="card-body">
+        <div style={{color:'red'}}>{status}</div>
         {show ? (
+
+
           <>
         <h5 class="card-title">Create Account</h5>
         <form>
@@ -47,17 +64,38 @@ function CreateAccount(){
           <label for="exampleInputName" class="form-label">Name</label>
           <input type="input" class="form-control" id="name" placeholder="Enter Name" value={name} onChange={e => setName(e.currentTarget.value)}/>
         </div>
+
+
+
         <div className="mb-3">
           <label for="exampleInputEmail1" class="form-label">Email Address</label>
           <input type="input" class="form-control" id="email" placeholder="Enter Email" value={email} onChange={e => setEmail(e.currentTarget.value)}/>
         </div>
+
+
+
+
         <div className="mb-3">
           <label for="exampleInputPassword1" class="form-label">Password</label>
           <input type="input" class="form-control" id="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.currentTarget.value)}/>
         </div>
-          <button type="submit" class="btn btn-light" onClick={handleCreate}>Create Account</button>
+
+          {isEnabladed ?
+          (<button type="submit" class="btn btn-light" onClick={handleCreate} >Create Account</button>)
+          :
+          (<button type="submit" class="btn btn-light" onClick={handleCreate} disabled>Create Account</button>)
+          }
+
         </form>
         </>
+
+
+
+
+
+
+
+
         ):(
           <>
           <h5>Success</h5>
@@ -67,7 +105,7 @@ function CreateAccount(){
       </div>
     </div>
   )
-
+  
 
   }
 
--- src/components/deposit.js
+++ src/components/deposit.js
@@ -1,8 +1,68 @@
+import React from 'react';
+import UserContext from './context'
+
+
+
 function Deposit(){
+  const [status, setStatus] = React.useState('');
+  const [deposit, setDeposit] = React.useState('');
+  const [balance, setBalance] = React.useState(0);
+  const ctx = React.useContext(UserContext);
+  const isEnabladed = fieldsUsed()
+
+  function fieldsUsed(){
+    return(
+      deposit.length > 0
+    )
+  }
+
+  function validate(){
+    if (isNaN(deposit)){
+      setStatus('Not a number! Please enter number only');
+      setTimeout(() => setStatus(''),3000);
+      return false;
+    }
+    if (deposit<0){
+      setStatus('Negative number! Please enter positive number only');
+      setTimeout(() => setStatus(''),3000);
+      return false;
+    }
+      return true;
+    }
+
+  function handleCreate(){
+    console.log(deposit);
+    if (!validate(deposit, 'deposit')) return;
+    setBalance(balance + parseInt(deposit));
+    ctx.users.push({deposit});
+    setStatus('');
+    setDeposit('')
+  }
+
   return (
     <>
-       <div className="card mb-3" style={{maxWidth: "540px"}}>
-      </div>
+    <div className="card mb-3" style={{maxWidth: "540px", backgroundColor: 'rgb(230, 255, 238'}}>
+      <div class="card-body"></div>
+       <div style={{color:'red'}}>{status}</div>
+
+          <h5 class="card-title">Deposit Money</h5>
+          <form>
+          <div className="mb-3">
+    
+            <label class="form-label">Balance: £{balance}</label>
+            <input type="input" class="form-control" id="deposit" placeholder="Enter amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)}/>
+          </div>
+          
+          {isEnabladed ?
+          (<button type="submit" class="btn btn-light" onClick={handleCreate} >Deposit</button>)
+          :
+          (<button type="submit" class="btn btn-light" onClick={handleCreate} disabled>Deposit</button>)
+          }
+          
+
+        </form>
+
+        </div>
     </>
   )
 }
